001bf057e697165e87898e884da94af7650b9dbd
zhanghua <zhanghua123@gmail.com>
2026-09-01 20:26:42 +0800

    Ignore VuePress generated files

--- docs/.vuepress/.temp/markdown-math/config.js
+++
@@ -1,2 +0,0 @@
-import "C:/Users/Administrator/Desktop/workspace/optimibi/docs/node_modules/katex/dist/katex.min.css";
-import "C:/Users/Administrator/Desktop/workspace/optimibi/docs/node_modules/@vuepress/plugin-markdown-math/lib/client/styles/katex.css";
--- docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,3 +0,0 @@
-<template><div><meta http-equiv="refresh" content="0; url=https://example.com"></div></template>
-
-
